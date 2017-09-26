624d4d5ee44327a2748a531e2d02d5d0005796a8
Jaimelll <jaduran0102@gmail.com>
2017-09-26 09:37:27 -0500

    primer repo

---
+++ app/admin/dashboard.rb
@@ -0,0 +1,10 @@
+ActiveAdmin.register_page "Dashboard" do
+
+  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+
+  content title: proc{ I18n.t("active_admin.dashboard") } do
+
+
+  link_to 'calendario', events_path
+  end # content
+end
